e26d171596dabcd3a36d47cb38cd8561bba5d7e1
Parduman12 <pardumanxrajrana@gmail.com>
2026-07-12 15:38:42 +0530

    error resolvej

--- frontend/src/features/auth/hooks/useAuth.js
+++ frontend/src/features/auth/hooks/useAuth.js
@@ -1,67 +1,67 @@
 import { useContext, useEffect } from "react";
 import { AuthContext } from "../auth.context.jsx";
-import { login, logout, getMe, register } from "../Services/auth.api.js";
+import { login, logout, getMe, register } from "../services/auth.api.js";
 
-export const useAuth = ()=>{
-    const {user, setUser, loading, setLoading} = useContext(AuthContext);
+export const useAuth = () => {
+    const { user, setUser, loading, setLoading } = useContext(AuthContext);
 
     useEffect(() => {
-        const fetchUser = async()=>{
-            
+        const fetchUser = async () => {
+
             try {
                 const data = await getMe();
-                if(data) setUser(data.user);                
+                if (data) setUser(data.user);
             } catch (error) {
                 console.error(error)
             }
-            finally{
+            finally {
                 setLoading(false);
             }
         }
         fetchUser();
-      
-    }, [])
-    
 
-    const handleLogin = async({email, password})=>{
+    }, [])
+
+
+    const handleLogin = async ({ email, password }) => {
         try {
             setLoading(true);
-            const data = await login({email, password});
+            const data = await login({ email, password });
             setUser(data.user);
-            
+
         } catch (error) {
             console.error("There is some error while posting login", error)
         }
-        finally{
+        finally {
             setLoading(false);
         }
 
     }
-    const handleRegister = async({username, email, password}) => {
+    const handleRegister = async ({ username, email, password }) => {
         try {
             setLoading(true);
-            const data = await register({username, email, password});
+            const data = await register({ username, email, password });
             setUser(data.user);
-            
+
         } catch (error) {
-            
+
         }
-        finally{
+        finally {
             setLoading(false);
         }
     }
-    const handleLogout = async() => {
+    const handleLogout = async () => {
         try {
             setLoading(true);
             const data = await logout();
-            setUser(null);            
+            setUser(null);
         } catch (error) {
-            
+
         }
-        finally{
+        finally {
             setLoading(false);
         }
     }
-    return {user, loading, handleLogin, handleLogout, handleRegister};
-    
+    return { user, loading, handleLogin, handleLogout, handleRegister };
+
 }
